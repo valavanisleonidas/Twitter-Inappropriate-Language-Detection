d0e755d7d6f40a67451034872e9fe5482803eada
Leonidas Valavanis <valavanisleonidas@gmail.com>
2019-10-21 01:03:03 +0200

    add twitter config

---
+++ kafka-producer/twitter_config.py
@@ -0,0 +1,7 @@
+#These information comes from Twitter API
+
+consumer_key = '(add-your-credentials)'
+consumer_secret = '(add-your-credentials)'
+access_token = '(add-your-credentials)'
+access_secret = '(add-your-credentials)'
+
